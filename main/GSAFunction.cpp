4b7b6c55534bcf9be08ae3de4efcce11e4cadf90
giovanni <giovanni@localhost.localdomain>
2018-12-02 16:31:13 +0100

    fx spread as indipendent risk factor

--- main/GSAFunction.cpp
+++ main/GSAFunction.cpp
@@ -37,6 +37,9 @@ GSAFunction::GSAFunction(std::string todayAsString){
 	fillCurve(tradeFile, assetCurveValues, offsets_assetCurve, periods_assetCurve, dates_assetCurve);
 	std::vector<double> numeraireCurveValues;
 	fillCurve(tradeFile, numeraireCurveValues, offsets_numeraireCurve, periods_numeraireCurve, dates_numeraireCurve);
+	//leggo curva spread
+	std::vector<double> spreadCurveValues;
+	fillCurve(tradeFile, spreadCurveValues, offsets_spreadCurve, periods_spreadCurve, dates_spreadCurve);
 	//leggo fx Spot:
 	std::cout << "Reading fx rate from the trade file..." << std::endl;
 	std::getline(tradeFile,line);
@@ -52,13 +55,13 @@ GSAFunction::GSAFunction(std::string todayAsString){
 	//estraggo dalla curva i parametri del modello
 	//
 	//	prendo come proxy dello short rate il tasso a 1 g	
-	std::shared_ptr<YieldTermStructure> ccy1_ts=createTermStructure(ccy1CurveValues, offsets_ccy1);
+	ext::shared_ptr<YieldTermStructure> ccy1_ts=createTermStructure(ccy1CurveValues, offsets_ccy1);
 	ccy1_r0=ccy1_ts->zeroRate(1/365,Compounding::Continuous,Annual,true);
-	std::shared_ptr<YieldTermStructure> ccy2_ts=createTermStructure(ccy2CurveValues, offsets_ccy2);
+	ext::shared_ptr<YieldTermStructure> ccy2_ts=createTermStructure(ccy2CurveValues, offsets_ccy2);
 	ccy2_r0=ccy2_ts->zeroRate(1/365,Compounding::Continuous,Annual,true);
-	std::shared_ptr<YieldTermStructure> assetCurve_ts=createTermStructure(assetCurveValues, offsets_assetCurve);
+	ext::shared_ptr<YieldTermStructure> assetCurve_ts=createTermStructure(assetCurveValues, offsets_assetCurve);
 	assetCurve_r0=assetCurve_ts->zeroRate(1/365,Compounding::Continuous,Annual,true);
-	std::shared_ptr<YieldTermStructure> numeraireCurve_ts=createTermStructure(numeraireCurveValues, offsets_numeraireCurve);
+	ext::shared_ptr<YieldTermStructure> numeraireCurve_ts=createTermStructure(numeraireCurveValues, offsets_numeraireCurve);
 	numeraireCurve_r0=numeraireCurve_ts->zeroRate(1/365,Compounding::Continuous,Annual,true);
 
 	rateModel_ccy1=std::shared_ptr<OneFactorAffineModel>(new Vasicek(ccy1_r0));
@@ -72,6 +75,15 @@ GSAFunction::GSAFunction(std::string todayAsString){
 	sr_calibrator.calibrate(offsets_assetCurve,assetCurveValues,rateModel_assetCurve,today,assetCurve_r0);
 	sr_calibrator.calibrate(offsets_numeraireCurve,numeraireCurveValues,rateModel_numeraireCurve,today,numeraireCurve_r0);
 
+	//la curva degli spread viene trattata come una curva di tassi
+	//
+	//	prendo come proxy dello short rate il tasso a 1 g
+		
+	ext::shared_ptr<YieldTermStructure> spread_ts=createTermStructure(spreadCurveValues, offsets_spreadCurve);
+	spreadCurve_r0=spread_ts->zeroRate(1/365,Compounding::Continuous,Annual,true);
+
+	model_spreadCurve=std::shared_ptr<OneFactorAffineModel>(new Vasicek(spreadCurve_r0));
+	sr_calibrator.calibrate(offsets_spreadCurve,spreadCurveValues,model_spreadCurve,today,spreadCurve_r0);
 	//modello per il tasso di cambio
 	Handle<Quote> h1(ext::make_shared<SimpleQuote>(SimpleQuote(ccy1_r0)));
 	Handle<Quote> h2(ext::make_shared<SimpleQuote>(SimpleQuote(0.30)));
@@ -95,6 +107,7 @@ double GSAFunction::eval(double ccy1_dw,
 		double ccy2_dw, 
 		double assetCurve_dw, 
 		double numeraireCurve_dw,
+		double spreadCurve_dw,
 		double fx_dw,
 		double t) const{
 
@@ -107,13 +120,14 @@ double GSAFunction::eval(double ccy1_dw,
 
 	double numeraireCurve_r = rateModel_numeraireCurve->dynamics()->process()->evolve(0, numeraireCurve_r0, t, numeraireCurve_dw);
 
+	double spreadCurve_r = model_spreadCurve->dynamics()->process()->evolve(0, spreadCurve_r0, t, spreadCurve_dw);
 	//2.calcolo gli zero rates delle varie curve
 
 	Handle<YieldTermStructure> ccy1_ts(createTermStructure(ccy1_r, rateModel_ccy1, offsets_ccy1, periods_ccy1, dates_ccy1));
 	Handle<YieldTermStructure> ccy2_ts(createTermStructure(ccy2_r, rateModel_ccy2, offsets_ccy2, periods_ccy2, dates_ccy2));
 	Handle<YieldTermStructure> assetCurve_ts(createTermStructure(assetCurve_r, rateModel_assetCurve, offsets_assetCurve, periods_assetCurve, dates_assetCurve));
-	Handle<YieldTermStructure> numeraireCurve_ts(createTermStructure(numeraireCurve_r, rateModel_numeraireCurve, offsets_numeraireCurve, periods_numeraireCurve, dates_numeraireCurve));
-
+	//2a. sommo spread con curva USD per ottenere la numeraire curve
+	Handle<YieldTermStructure> numeraireCurve_ts(createTermStructureWithSpread(numeraireCurve_r, rateModel_numeraireCurve, periods_numeraireCurve, dates_numeraireCurve, spreadCurve_r, model_spreadCurve, periods_spreadCurve, dates_spreadCurve));
 	//3.calcolo il tasso di cambio in base al modello
 	//
 	double fx = process_fx->evolve(0, fx_spot, t, fx_dw);
@@ -145,14 +159,14 @@ ext::shared_ptr<YieldTermStructure> GSAFunction::createTermStructure(double rate
 
 };
 
-std::shared_ptr<YieldTermStructure> GSAFunction::createTermStructure(std::vector<double> rates, std::vector<double> offsets){
+ext::shared_ptr<YieldTermStructure> GSAFunction::createTermStructure(const std::vector<double> rates,const std::vector<double> offsets) const{
 	Calendar calendar = TARGET();
 	std::vector<Date> dates;
 	for (size_t i = 0; i < offsets.size(); ++i ){
 
 		dates.push_back(calendar.adjust(calendar.advance(settlementDate,offsets[i],Days)));
 	}
-	std::shared_ptr<YieldTermStructure> termStructure(
+	ext::shared_ptr<YieldTermStructure> termStructure(
 			new InterpolatedZeroCurve<Linear>(
 				dates, rates,
 				dayCount_,
@@ -161,6 +175,39 @@ std::shared_ptr<YieldTermStructure> GSAFunction::createTermStructure(std::vector
 
 };
 
+ext::shared_ptr<YieldTermStructure> GSAFunction::createTermStructureWithSpread(double rate,const std::shared_ptr<OneFactorAffineModel> & rateModel, const std::vector<Time> & curveTimes, const std::vector<Date> & dates, double spreadRate,const std::shared_ptr<OneFactorAffineModel> & spreadModel, const std::vector<Time> & spreadCurveTimes, const std::vector<Date> & spreadDates) const{
+
+	Calendar calendar = TARGET();
+	std::vector<double> spreadRates;
+	spreadRates.push_back(0.0);
+
+	for (size_t i = 0; i < spreadCurveTimes.size(); ++i ){
+
+		double bondDiscount = spreadModel->discountBond(0,spreadCurveTimes[i],spreadRate);
+		Real modelRate = (1 - bondDiscount) / (bondDiscount * curveTimes[i]);
+		spreadRates.push_back(modelRate);
+	}
+	InterpolatedZeroCurve<Linear> spreadTermStructure(
+				spreadDates, spreadRates,
+				dayCount_,
+				calendar);
+	std::vector<double> rates;
+	rates.push_back(0.0);
+	for (size_t i = 0; i < curveTimes.size(); ++i ){
+		
+		double bondDiscount = rateModel->discountBond(0,curveTimes[i],rate);
+		Real modelRate = (1 - bondDiscount) / (bondDiscount * curveTimes[i]);
+		modelRate += spreadTermStructure.zeroRate(curveTimes[i],Compounding::Continuous, Annual, true);
+		rates.push_back(modelRate);
+	}
+	ext::shared_ptr<YieldTermStructure> termStructure(
+			new InterpolatedZeroCurve<Linear>(
+				dates, rates,
+				dayCount_,
+				calendar));
+	return termStructure;
+
+};
 void GSAFunction::fillCurve(std::ifstream & file, std::vector<double> & curveValues, std::vector<double> & curveOffsets, std::vector<Time> & times, std::vector<Date> & dates){
 	Calendar calendar = TARGET();
 	std::string line;
--- main/GSAFunction.hpp
+++ main/GSAFunction.hpp
@@ -13,23 +13,28 @@ class GSAFunction{
 		std::shared_ptr<QuantLib::OneFactorAffineModel> rateModel_ccy2;
 		std::shared_ptr<QuantLib::OneFactorAffineModel> rateModel_assetCurve;
 		std::shared_ptr<QuantLib::OneFactorAffineModel> rateModel_numeraireCurve;
+		std::shared_ptr<QuantLib::OneFactorAffineModel> model_spreadCurve;
 		std::shared_ptr<QuantLib::BlackScholesProcess> process_fx;
 		std::vector<double> offsets_ccy1;
 		std::vector<double> offsets_ccy2;
 		std::vector<double> offsets_assetCurve;
 		std::vector<double> offsets_numeraireCurve;
+		std::vector<double> offsets_spreadCurve;
 		std::vector<QuantLib::Time> periods_ccy1;
 		std::vector<QuantLib::Time> periods_ccy2;
 		std::vector<QuantLib::Time> periods_assetCurve;
 		std::vector<QuantLib::Time> periods_numeraireCurve;
+		std::vector<QuantLib::Time> periods_spreadCurve;
 		std::vector<QuantLib::Date> dates_ccy1;
 		std::vector<QuantLib::Date> dates_ccy2;
 		std::vector<QuantLib::Date> dates_assetCurve;
 		std::vector<QuantLib::Date> dates_numeraireCurve;
+		std::vector<QuantLib::Date> dates_spreadCurve;
 		double ccy1_r0;
 		double ccy2_r0;
 		double assetCurve_r0;
 		double numeraireCurve_r0;
+		double spreadCurve_r0;
 		double fx_spot;
 		QuantLib::Actual365Fixed dayCount_;
 		CCSPricer ccsPricer;
@@ -40,8 +45,16 @@ class GSAFunction{
 				const std::vector<QuantLib::Time> & curveTimes, 
 				const std::vector<QuantLib::Date> & dates) const;
 
-		std::shared_ptr<QuantLib::YieldTermStructure> createTermStructure(std::vector<double> rates, 
-				std::vector<double> offsets);
+		QuantLib::ext::shared_ptr<QuantLib::YieldTermStructure> createTermStructure(const std::vector<double> rates, 
+				const std::vector<double> offsets) const;
+		QuantLib::ext::shared_ptr<QuantLib::YieldTermStructure> createTermStructureWithSpread(double rate, 
+				const std::shared_ptr<QuantLib::OneFactorAffineModel> & rateModel, 
+				const std::vector<QuantLib::Time> & curveTimes, 
+				const std::vector<QuantLib::Date> & dates, 
+				double spreadRate,
+				const std::shared_ptr<QuantLib::OneFactorAffineModel> & spreadModel, 
+				const std::vector<QuantLib::Time> & spreadCurveTimes, 
+				const std::vector<QuantLib::Date> & spreadDates) const;
 		void fillCurve(std::ifstream & file, std::vector<double> & curveValues, std::vector<double> & curveOffsets,  std::vector<QuantLib::Time> & times, std::vector<QuantLib::Date> & dates);
 	public: 
 		GSAFunction(std::string todayAsString);
@@ -50,6 +63,7 @@ class GSAFunction{
 				double ccy2_r, 
 				double asset_r, 
 				double numeraire_r,
+				double spreadCurve_dw,
 				double fx,
 				double t) const;
 	
