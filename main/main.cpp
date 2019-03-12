da5bbfa1738c79050f6f84b9d14f03f0541e5a1b
giovanni <giovanni@localhost.localdomain>
2019-03-13 00:47:28 +0100

    13/03/19

--- main/main.cpp
+++ main/main.cpp
@@ -20,11 +20,18 @@ int main (int argc,char* argv[]){
 		[&gsa_function] (std::vector<double> v) {return gsa_function.eval(v[0],v[1], v[2], v[3], v[4], v[5], 1);};
 	std::cout << "Base price: " << gsa_function.eval(0.0,0.0,0.0,0.0,0.0,0.0,0) << std::endl;
 	int sampleNumber = stoi(conf.get("sample_number"));
-	PseudoRandomGaussianGenerator rng;
 	shared_ptr<GSAResults> gsa_results(new GSAResults());
-	GlobalSensitivityCalculator<PseudoRandomGaussianGenerator> gsa_calculator(rng,f,sampleNumber,6, gsa_results);
 	std::vector<int> indexes = gsa_function.getIndices();
-	gsa_calculator.calculate(std::set<int>(indexes.begin(),indexes.end()));
+	if(conf.get("mc_mode") == "PRG"){
+		PseudoRandomGaussianGenerator rng;
+		GlobalSensitivityCalculator<PseudoRandomGaussianGenerator> gsa_calculator(rng,f,sampleNumber,6, gsa_results);
+		gsa_calculator.calculate(std::set<int>(indexes.begin(),indexes.end()));
+	} else {
+		QuasiRandomGaussianGenerator qrng(1);
+		GlobalSensitivityCalculator<QuasiRandomGaussianGenerator> gsa_calculator(qrng,f,sampleNumber,6, gsa_results);
+		gsa_calculator.calculate(std::set<int>(indexes.begin(),indexes.end()));
+	}
+
 	duration = (std::clock() - start)/(double) (CLOCKS_PER_SEC/1000);
 	cout << "Calculation time: " << duration << endl;
 	cout << "D_x: " << gsa_results->getD_x() << endl;
